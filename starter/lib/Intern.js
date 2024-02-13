2d7a7494d28ae1ce8f9eb0d5e084d59d248ad325
JoeCross <krstevskigj@gmail.com>
2024-02-13 15:47:40 +0000

    adding all the classes after they passed the tests

--- starter/lib/Intern.js
+++ starter/lib/Intern.js
@@ -1,1 +1,18 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Intern extends Employee{
+    constructor(name = "", employeeID = 0, email = "", school = ""){
+        super(name, employeeID, email)
+
+        this.school = school;
+    }
+
+    getRole(){
+        return 'Intern';
+    }
+    getSchool(){
+        return this.school;
+    }
+}
+
+module.exports = Intern;
